98806029bd482f61284db108003a64c84a47dcba
Denis <denis.were@student.moringaschool.com>
2023-04-11 20:54:45 +0300

    assignment

--- index.js
+++ index.js
@@ -1,5 +1,94 @@
-/* Your Code Here */
-
+class Employee {
+    constructor(firstName, familyName, title, payPerHour) {
+      this.firstName = firstName;
+      this.familyName = familyName;
+      this.title = title;
+      this.payPerHour = payPerHour;
+      this.timeInEvents = [];
+      this.timeOutEvents = [];
+    }
+    
+    hoursWorkedOnDate(date) {
+      const timeInEvent = this.timeInEvents.find(event => event.date === date);
+      const timeOutEvent = this.timeOutEvents.find(event => event.date === date);
+      
+      if (!timeInEvent || !timeOutEvent) {
+        return 0;
+      }
+      
+      const hoursWorked = (timeOutEvent.hour - timeInEvent.hour) / 100;
+      return hoursWorked;
+    }
+    
+    wagesEarnedOnDate(date) {
+      const hoursWorked = this.hoursWorkedOnDate(date);
+      const wagesEarned = hoursWorked * this.payPerHour;
+      return wagesEarned;
+    }
+  }
+  
+  function createEmployeeRecord(employeeData) {
+    const employee = new Employee(
+      employeeData[0], 
+      employeeData[1], 
+      employeeData[2], 
+      employeeData[3]
+    );
+    
+    return employee;
+  }
+  
+  function createEmployeeRecords(employeesData) {
+    const employees = employeesData.map(employeeData => createEmployeeRecord(employeeData));
+    
+    return employees;
+  }
+  
+  function createTimeInEvent(employee, dateTimeString) {
+    const [date, hour] = dateTimeString.split(' ');
+    
+    const timeInEvent = {
+      type: 'TimeIn',
+      date: date,
+      hour: parseInt(hour, 10)
+    };
+    
+    employee.timeInEvents.push(timeInEvent);
+    
+    return employee;
+  }
+  
+  function createTimeOutEvent(employee, dateTimeString) {
+    const [date, hour] = dateTimeString.split(' ');
+    
+    const timeOutEvent = {
+      type: 'TimeOut',
+      date: date,
+      hour: parseInt(hour, 10)
+    };
+    
+    employee.timeOutEvents.push(timeOutEvent);
+    
+    return employee;
+  }
+  
+  
+  
+  function findEmployeeByFirstName(collection, firstName) {
+    const employee = collection.find(employee => employee.firstName === firstName);
+    
+    return employee;
+  }
+  
+  function calculatePayroll(employees) {
+    const totalWages = employees.reduce((total, employee) => {
+      const wages = allWagesFor(employee);
+      return total + wages;
+    }, 0);
+    
+    return totalWages;
+  }
+  
 /*
  We're giving you this function. Take a look at it, you might see some usage
  that's new and different. That's because we're avoiding a well-known, but
